ed75126480f3a1e5e50c57a4f2ade4810053af03
Haruki Yano <haruki.y.11g@gmail.com>
2022-06-14 16:09:51 +0900

    Refactor UnityVersionExpressionParser.

--- Assets/SmartAddresser/Runtime/Foundation/SemanticVersioning/UnityVersionExpressionParser.cs
+++ Assets/SmartAddresser/Runtime/Foundation/SemanticVersioning/UnityVersionExpressionParser.cs
@@ -25,82 +25,69 @@ namespace SmartAddresser.Runtime.Foundation.SemanticVersioning
 
         public CompositeVersionComparator CreateComparator(string expression)
         {
+            var split = expression.Split(',');
+            return split.Length switch
+            {
+                1 => CreateSingleVersionComparer(expression),
+                2 => CreateVersionRangeComparer(expression),
+                _ => throw new ArgumentException($"Invalid format expression: {expression}")
+            };
+        }
+
+        private static CompositeVersionComparator CreateSingleVersionComparer(string expression)
+        {
+            var result = new CompositeVersionComparator();
+            var regex = new Regex(@"^\[(.+)\]");
+            var match = regex.Match(expression);
+            var versionStr = match.Success ? match.Groups[1].Value : expression;
+            var comparatorOperator = match.Success
+                ? VersionComparator.Operator.Equal
+                : VersionComparator.Operator.GreaterThanOrEqual;
+
+            if (Version.TryCreate(versionStr, out var minVersion))
+                result.Add(new VersionComparator(minVersion, comparatorOperator));
+            else
+                throw new ArgumentException($"Invalid format expression: {expression}");
+
+            return result;
+        }
+
+        private static CompositeVersionComparator CreateVersionRangeComparer(string expression)
+        {
             var result = new CompositeVersionComparator();
             var split = expression.Split(',');
-            if (split.Length == 1)
-            {
-                var regex = new Regex(@"^\[(.+)\]");
-                var match = regex.Match(expression);
-                if (match.Success)
-                {
-                    // "[2.4.5]" is "x = 2.4.5" 
-                    var versionStr = match.Groups[1].Value;
-                    if (Version.TryCreate(versionStr, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.Equal));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-                else
-                {
-                    // "2.1.0-preview.7" is "x >= 2.1.0-preview.7"
-                    if (Version.TryCreate(expression, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.GreaterThanOrEqual));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-            }
-            else if (split.Length == 2)
-            {
-                // "[1.3,3.4.1]" is "1.3.0 <= x <= 3.4.1"
-                // "(1.3.0,3.4)" is "1.3.0 < x < 3.4.0"
-                // "[1.1,3.4)" is "1.1.0 <= x < 3.4.0"
-                // "(0.2.4,5.6.2-preview.2]" is "0.2.4 < x <= 5.6.2.-preview.2"
-                var firstChar = expression.Substring(0, 1);
-                var minVersionStr = split[0].Substring(1, split[0].Length - 1);
-                if (firstChar == "[")
-                {
-                    if (Version.TryCreate(minVersionStr, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.GreaterThanOrEqual));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-                else if (firstChar == "(")
-                {
-                    if (Version.TryCreate(minVersionStr, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.GreaterThan));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-                else
-                {
-                    throw new ArgumentException($"Invalid format expression: {expression}");
-                }
 
-                var lastChar = expression.Substring(expression.Length - 1, 1);
-                var maxVersionStr = split[1].Substring(0, split[1].Length - 1);
-                if (lastChar == "]")
-                {
-                    if (Version.TryCreate(maxVersionStr, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.LessThanOrEqual));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-                else if (lastChar == ")")
-                {
-                    if (Version.TryCreate(maxVersionStr, out var version))
-                        result.Add(new VersionComparator(version, VersionComparator.Operator.LessThan));
-                    else
-                        throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-                else
-                {
-                    throw new ArgumentException($"Invalid format expression: {expression}");
-                }
-            }
-            else
+            // Create minimum version comparer.
+            var firstChar = expression[0];
+            var minVersionStr = split[0].Substring(1, split[0].Length - 1);
+
+            var minVersionOperator = firstChar switch
             {
+                '[' => VersionComparator.Operator.GreaterThanOrEqual,
+                '(' => VersionComparator.Operator.GreaterThan,
+                _ => throw new ArgumentException($"Invalid format expression: {expression}")
+            };
+
+            if (Version.TryCreate(minVersionStr, out var minVersion))
+                result.Add(new VersionComparator(minVersion, minVersionOperator));
+            else
                 throw new ArgumentException($"Invalid format expression: {expression}");
-            }
+
+            // Create max version comparer.
+            var lastChar = expression[expression.Length - 1];
+            var maxVersionStr = split[1].Substring(0, split[1].Length - 1);
+
+            var maxVersionOperator = lastChar switch
+            {
+                ']' => VersionComparator.Operator.LessThanOrEqual,
+                ')' => VersionComparator.Operator.LessThan,
+                _ => throw new ArgumentException($"Invalid format expression: {expression}")
+            };
+
+            if (Version.TryCreate(maxVersionStr, out var maxVersion))
+                result.Add(new VersionComparator(maxVersion, maxVersionOperator));
+            else
+                throw new ArgumentException($"Invalid format expression: {expression}");
 
             return result;
         }
