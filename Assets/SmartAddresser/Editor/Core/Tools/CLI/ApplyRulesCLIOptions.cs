188e788fd16b2ea18ba937ae05d5c7e27bf707e8
Haruki Yano <haruki.y.11g@gmail.com>
2022-12-12 12:00:34 +0900

    Add CLI.

---
+++ Assets/SmartAddresser/Editor/Core/Tools/CLI/ApplyRulesCLIOptions.cs
@@ -0,0 +1,40 @@
+using SmartAddresser.Editor.Foundation;
+
+namespace SmartAddresser.Editor.Core.Tools.CLI
+{
+    public sealed class ApplyRulesCLIOptions
+    {
+        private const string LayoutRuleAssetPathArgName = "-layoutRuleAssetPath";
+        private const string DoValidateArgName = "-validate";
+        private const string ResultFilePathArgName = "-resultFilePath";
+        private const string FailWhenWarningArgName = "-failWhenWarning";
+        private const string DefaultResultFilePathWithoutExtensions = "SmartAddresser/validate_result";
+
+        public string LayoutRuleAssetPath { get; private set; }
+        public bool DoValidate { get; private set; }
+        public string ResultFilePath { get; private set; }
+        public bool FailWhenWarning { get; private set; }
+
+        public static ApplyRulesCLIOptions CreateFromCommandLineArgs()
+        {
+            var options = new ApplyRulesCLIOptions();
+
+            // Layout Rule Asset Path
+            if (CommandLineUtility.TryGetStringValue(LayoutRuleAssetPathArgName, out var layoutRuleAssetPath))
+                options.LayoutRuleAssetPath = layoutRuleAssetPath;
+
+            // Result File Path
+            if (!CommandLineUtility.TryGetStringValue(ResultFilePathArgName, out var resultFilePath))
+                resultFilePath = $"{DefaultResultFilePathWithoutExtensions}.json";
+            options.ResultFilePath = resultFilePath;
+
+            // Do Validate
+            options.DoValidate = CommandLineUtility.Contains(DoValidateArgName);
+
+            // Fail When Warning
+            options.FailWhenWarning = CommandLineUtility.Contains(FailWhenWarningArgName);
+
+            return options;
+        }
+    }
+}
