7d225c99bffbd37421bacbcc48e0bd4d39eef4b0
shunsuke-saito-mummy <saito_shunsuke@colorfulpalette.co.jp>
2023-12-23 17:22:19 +0900

    add SortByAssetPaths parameter to SmartAddresserProjectSetttings

--- Assets/SmartAddresser/Editor/Core/Tools/Shared/SmartAddresserProjectSettingsProvider.cs
+++ Assets/SmartAddresser/Editor/Core/Tools/Shared/SmartAddresserProjectSettingsProvider.cs
@@ -82,6 +82,13 @@ namespace SmartAddresser.Editor.Core.Tools.Shared
                         projectSettings.ValidationSettings = new SmartAddresserProjectSettings.Validation(duplicateAddresses,
                             duplicateAssetPaths, entryHasMultipleVersions);
                 }
+
+                using (var ccs = new EditorGUI.ChangeCheckScope())
+                {
+                    var sortByAssetPaths = EditorGUILayout.Toggle("Sort by assetPaths", projectSettings.SortByAssetPaths);
+                    if (ccs.changed)
+                        projectSettings.SortByAssetPaths = sortByAssetPaths;
+                }
             }
         }
 
